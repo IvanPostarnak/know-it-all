acffa295a256acb8e9152264a683e815a8262f8b
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-02-14 15:31:25 +0300

    feat: add small checking functions and small interactivity

--- blocks/add-card-form/add-card-form.mjs
+++ blocks/add-card-form/add-card-form.mjs
@@ -7,6 +7,24 @@ export default function addCardIntoDatabase() {
 
   cardForm.addEventListener("submit", (elem) => {
     elem.preventDefault();
+    if (isAppropriateQuestion(cardQuestion) === false) {
+      cardQuestion.focus();
+      alert('Question field is empty!');
+      return;
+    }
+    if (isAppropriateAnswer(cardAnswer) === false) {
+      cardAnswer.focus();
+      alert('Answer field is empty!');
+      return;
+    }
     alert('Вы нажали на кнопку');
   })
+}
+
+function isAppropriateQuestion(questionValue) {
+  return questionValue.value !== '';
+}
+
+function isAppropriateAnswer(answerValue) {
+  return answerValue.value !== '';
 }
