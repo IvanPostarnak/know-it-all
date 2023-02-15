e9fbf980de0d6b09b21bb9423141dd0610cff671
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-02-15 08:05:55 +0300

    feat: add dynamic filling of select sections (temp version)

--- blocks/add-card-form/add-card-form.mjs
+++ blocks/add-card-form/add-card-form.mjs
@@ -1,4 +1,13 @@
+const tempCategories = {
+  'html': 'HTML',
+  'css': 'CSS',
+  'js': 'JavaScript',
+}
+
+
 export default function addCardIntoDatabase() {
+  createSelectElementsForCategories();
+  
   const cardForm = document.querySelector('.js-cardForm');
 
   cardForm.addEventListener("submit", (elem) => {
@@ -13,14 +22,57 @@ export default function addCardIntoDatabase() {
   })
 }
 
+
+function createSelectElementsForCategories() {
+  const arrayOfSelectHolders = document.querySelectorAll('.add-card-form__select-holder');
+  
+  const mainSelect = document.createElement('select');
+  mainSelect.classList.add('add-card-form__select-field', 'add-card-form__select-field--main-catefory');
+  const mainSelectPlaceholder = document.createElement('option');
+  mainSelectPlaceholder.value = "";
+  mainSelectPlaceholder.text = 'main category';
+  mainSelectPlaceholder.setAttribute('selected', '');
+  mainSelectPlaceholder.setAttribute('disabled', '');
+  mainSelectPlaceholder.setAttribute('hidden', '');
+  mainSelect.append(mainSelectPlaceholder);
+
+  for (let id in tempCategories) {
+    const option = document.createElement('option');
+    option.value = id;
+    option.text = tempCategories[id];
+    option.classList.add('add-card-form__select-item');
+    mainSelect.append(option);
+  }
+
+  const additionalSelect = document.createElement('select');
+  additionalSelect.classList.add('add-card-form__select-field', 'add-card-form__select-field--additional-categories');
+  additionalSelect.setAttribute('mmultiple', '');
+  const additionalSelectPlaceholder = document.createElement('option');
+  additionalSelectPlaceholder.value = "";
+  additionalSelectPlaceholder.text = 'additional categories';
+  additionalSelectPlaceholder.setAttribute('selected', '');
+  additionalSelectPlaceholder.setAttribute('disabled', '');
+  additionalSelectPlaceholder.setAttribute('hidden', '');
+  additionalSelect.append(additionalSelectPlaceholder);
+
+  for (let id in tempCategories) {
+    const option = document.createElement('option');
+    option.value = id;
+    option.text = tempCategories[id];
+    option.classList.add('add-card-form__select-item');
+    additionalSelect.append(option);
+  }
+
+  arrayOfSelectHolders[0].append(mainSelect);
+  arrayOfSelectHolders[1].append(additionalSelect);
+}
+
 async function saveData(data) {
   const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
     method: 'POST',
     body: data,
   });
-
   await validateResponse(response);
-
   return response.status;
 }
 
