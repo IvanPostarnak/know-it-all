a9097f41153d5456be70d59cdf2fd1b6b94165d9
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-02-16 08:14:25 +0300

    refactor: changed names from 'preloader' to 'oneFieldPreloader'

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
 import bubbleGlide from './../blocks/bubble/bubble.mjs'
-import addCardForm from './../blocks/add-card-form/add-card-form.mjs'
+import addCardForm from './../blocks/add-card-form/addCardForm.mjs'
 bubbleGlide();
 addCardForm();
