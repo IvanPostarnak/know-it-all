16fe048fd43c5665836b262f9e3f053864953d09
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-02-17 04:41:11 +0300

    feat: add 'index.php' server (beta version)

---
+++ server/index.php
@@ -0,0 +1,8 @@
+<?php
+  define(USERS_CARDS_FILE_URL, '/database/usersCards.json');
+  define(USERS_CATEGORIES_FILE_URL, '/database/usersCategories.json');
+  define(DEFAULT_CARDS_FILE_URL, '/database/defaultCards.json');
+  define(DEFAULT_CATEGORIES_FILE_URL, '/database/defaultCategories.json');
+  
+  
+?>
