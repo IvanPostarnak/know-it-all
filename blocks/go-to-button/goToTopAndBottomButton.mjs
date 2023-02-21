9b241b617b804994d0acf4dbdab4e919416856bc
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-02-21 06:24:41 +0300

    fix: fix js logics of 'go-to-button' rotation

--- blocks/go-to-button/goToTopAndBottomButton.mjs
+++ blocks/go-to-button/goToTopAndBottomButton.mjs
@@ -3,15 +3,21 @@ export default function goToTopAndBottomButton() {
   const bottomCoordinates = document.documentElement.scrollHeight;
   let destination = "bot";
 
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > bottomCoordinates / 2) {
+      goToButton.style.transform = "rotate(180deg)";
+      destination = "top"
+    } else {
+      goToButton.style.transform = "rotate(0deg)";
+      destination = "bot"
+    }
+  })
+
   goToButton.addEventListener('click', () => {
     if (destination === "bot") {
       window.scrollTo(0 , bottomCoordinates);
-      goToButton.style.transform = "rotate(180deg)";
-      destination = "top"
     } else if (destination === "top") {
       window.scrollTo(0 , 0);
-      goToButton.style.transform = "rotate(0deg)";
-      destination = "bot"
     }
   });
 }
