02b97cf9e806d1941593cbdd007789280614bb03
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-02-21 05:28:49 +0300

    feat: extract 'go-to-button' into separate block

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 import bubbleGlide from './../blocks/bubble/bubble.mjs'
 import addCardForm from './../blocks/add-card-form/addCardForm.mjs'
 import renderCards from './../blocks/single-card/renderCards.mjs'
-import goToTopAndBottomButton from './goToTopAndBottomButton.mjs';
+import goToTopAndBottomButton from '../blocks/go-to-button/goToTopAndBottomButton.mjs';
 
 bubbleGlide();
 addCardForm();
