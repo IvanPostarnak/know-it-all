119be15d5b8c8be9a4bcccbbb226762eb2a341b1
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-02-21 05:53:46 +0300

    fix: add arrows to top and buttom in 'go-to-button'

--- blocks/go-to-button/goToTopAndBottomButton.mjs
+++ blocks/go-to-button/goToTopAndBottomButton.mjs
@@ -6,9 +6,13 @@ export default function goToTopAndBottomButton() {
   goToButton.addEventListener('click', () => {
     if (destination === "bot") {
       window.scrollTo(0 , bottomCoordinates);
+      goToButton.classList.remove("go-to-top-and-bottom-button--bot");
+      goToButton.classList.add("go-to-top-and-bottom-button--top");
       destination = "top"
     } else if (destination === "top") {
       window.scrollTo(0 , 0);
+      goToButton.classList.remove("go-to-top-and-bottom-button--top");
+      goToButton.classList.add("go-to-top-and-bottom-button--bot");
       destination = "bot"
     }
   });
