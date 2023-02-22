3ec4f73da9335c13b52d803bacfefd7a037972f6
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-02-22 06:30:02 +0300

    feat: make users cards appear and disappear according to the checkbox

--- scripts/filter.mjs
+++ scripts/filter.mjs
@@ -4,17 +4,10 @@ export default async function filterCards() {
   let shownArrayOfCards = Array.from(document.querySelectorAll('.js-single-card-holder'));
   const arrayOfCards = [...shownArrayOfCards];
   console.log(shownArrayOfCards);
-  console.log(arrayOfCards);
 
 
   const defaultCardsCheckbox = document.querySelector('.js-default-type');
   const usersCardsCheckbox = document.querySelector('.js-users-type');
-
-  let areDefaultCardsShown = defaultCardsCheckbox.checked;
-  let areUsersCardsShown = usersCardsCheckbox.checked;
-
-  console.log(areDefaultCardsShown);
-  console.log(areUsersCardsShown);
 
   defaultCardsCheckbox.addEventListener('change', () => {
     if (defaultCardsCheckbox.checked) {
@@ -33,11 +26,28 @@ export default async function filterCards() {
       })
     }
   })
+
+  usersCardsCheckbox.addEventListener('change', () => {
+    if (usersCardsCheckbox.checked) {
+      shownArrayOfCards.forEach(card => {
+        if (card.querySelector('.single-card__type').textContent === "users") {
+          card.style.display = "block";
+          card.style.visibility = "visible";
+        }
+      })
+    } else {
+      shownArrayOfCards.forEach(card => {
+        if (card.querySelector('.single-card__type').textContent === "users") {
+          card.style.display = "none";
+          card.style.visibility = "hidden";
+        }
+      })
+    }
+  })
 }
 
 async function getMetaOfDatabase() {
   const response = await fetch("/server/server.php?meta=1");
   const data = await response.json();
-  console.log(data);
   return data;
 }
