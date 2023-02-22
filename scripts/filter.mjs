caf0bc34620e7eaded9f4dbd6f6aae3db96f5850
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-02-22 05:50:33 +0300

    feat: add removing default cards if checkbox is unchecked

--- scripts/filter.mjs
+++ scripts/filter.mjs
@@ -1,8 +1,36 @@
 export default async function filterCards() {
   let metaObject = await getMetaOfDatabase();
 
-  const arrayOfCards = Array.from(document.querySelectorAll('.single-card'));
+  const shownArrayOfCards = document.querySelectorAll('.js-single-card-holder');
+  const arrayOfCards = [...shownArrayOfCards];
+  console.log(shownArrayOfCards);
   console.log(arrayOfCards);
+
+
+  const defaultCardsCheckbox = document.querySelector('.js-default-type');
+  const usersCardsCheckbox = document.querySelector('.js-users-type');
+
+  let areDefaultCardsShown = defaultCardsCheckbox.checked;
+  let areUsersCardsShown = usersCardsCheckbox.checked;
+
+  console.log(areDefaultCardsShown);
+  console.log(areUsersCardsShown);
+
+  defaultCardsCheckbox.addEventListener('change', () => {
+    if (defaultCardsCheckbox.checked) {
+      arrayOfCards.forEach(card => {
+        if (card.querySelector('.single-card__type').textContent === "default") {
+          shownArrayOfCards.append(card);
+        }
+      })
+    } else {
+      shownArrayOfCards.forEach(card => {
+        if (card.querySelector('.single-card__type').textContent === "default") {
+          card.remove();
+        }
+      })
+    }
+  })
 }
 
 async function getMetaOfDatabase() {
