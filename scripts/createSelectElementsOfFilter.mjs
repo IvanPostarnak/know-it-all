c4089040a041a882e5b93ab7d98ffe239b4db1ab
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-02-23 04:38:56 +0300

    fix: fix placeholder of category filter

--- scripts/createSelectElementsOfFilter.mjs
+++ scripts/createSelectElementsOfFilter.mjs
@@ -1,8 +1,6 @@
 export default async function createSelectElementsOfFilter(categories) {
   let arrayOfSelectHolderInFilter = document.querySelector('.js-select-filter-holder');
   const select = createSelect();
-  const selectPlaceholder = createSelectPlaceholder();
-  select.append(selectPlaceholder);
 
   let all = 0;
   let option;
@@ -20,7 +18,8 @@ export default async function createSelectElementsOfFilter(categories) {
   option.value = "All";
   option.text = `All (${all})`;
   option.classList.add('add-card-form__select-item');
-  select.append(option);
+  option.setAttribute('selected', '');
+  select.prepend(option);
 
   arrayOfSelectHolderInFilter.textContent = '';
   arrayOfSelectHolderInFilter.append(select);
@@ -32,14 +31,4 @@ function createSelect() {
   select.classList.add('add-card-form__select-field', 'js-category-filter');
   select.setAttribute('name', 'chooseCategory');
   return select;
-}
-
-function createSelectPlaceholder() {
-  let placeholder = document.createElement('option');
-  placeholder.value = "";
-  placeholder.text = 'choose category';
-  placeholder.setAttribute('selected', '');
-  placeholder.setAttribute('disabled', '');
-  placeholder.setAttribute('hidden', '');
-  return placeholder;
 }
