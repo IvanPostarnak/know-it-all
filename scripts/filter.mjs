eed8b727e6b3a6ac931c67337a0b8ec540fe666e
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-02-23 06:12:37 +0300

    feat: add 'reset fiter' button on filter

--- scripts/filter.mjs
+++ scripts/filter.mjs
@@ -3,16 +3,16 @@ import createSelectElementsOfFilter from "./createSelectElementsOfFilter.mjs";
 import isAppropriateCard from "./isAppropriateCard.mjs";
 
 export default async function filterCards() {
-  let metaObject = await getMetaOfDatabase();
 
+  let metaObject = await getMetaOfDatabase();
   await createSelectElementsOfFilter(metaObject['categories']);
 
   let cardsHolder = document.querySelector('.js-cards-holder');
   let shownArrayOfCards = Array.from(cardsHolder.querySelectorAll('.js-single-card-holder'));
   let trashbox = document.querySelector('.js-trash-box');
   let hiddenCards = Array.from(trashbox.querySelectorAll('.js-single-card-holder'));
-  console.log(shownArrayOfCards);
-  console.log(hiddenCards);
+  // console.log(shownArrayOfCards);
+  // console.log(hiddenCards);
 
   const defaultCardsCheckbox = document.querySelector('.js-default-type');
   const usersCardsCheckbox = document.querySelector('.js-users-type');
@@ -37,28 +37,11 @@ export default async function filterCards() {
       
     shownArrayOfCards = Array.from(cardsHolder.querySelectorAll('.js-single-card-holder'));
     hiddenCards = Array.from(trashbox.querySelectorAll('.js-single-card-holder'));
-    console.log(shownArrayOfCards);
-    console.log(hiddenCards);
+    // console.log(shownArrayOfCards);
+    // console.log(hiddenCards);
   })
 
   usersCardsCheckbox.addEventListener('change', () => {
-    // if (usersCardsCheckbox.checked) {
-    //   hiddenCards.forEach(card => {
-    //     if (
-    //       card.querySelector('.single-card__type').textContent === "users"
-    //       && (card.querySelector('.single-card__main-category').textContent === document.querySelector('.js-category-filter').value
-    //       || card.querySelector('.single-card__additional-categories').textContent === document.querySelector('.js-category-filter').value)
-    //     ) {
-    //       cardsHolder.prepend(card);
-    //     }
-    //   })
-    // } else {
-    //   shownArrayOfCards.forEach(card => {
-    //     if (card.querySelector('.single-card__type').textContent === "users") {
-    //       trashbox.prepend(card);
-    //     }
-    //   })
-    // }
     if (shownArrayOfCards.length) {
       shownArrayOfCards.forEach(card => {
         if (isAppropriateCard(card) === false) {
@@ -76,30 +59,13 @@ export default async function filterCards() {
     } 
     shownArrayOfCards = Array.from(cardsHolder.querySelectorAll('.js-single-card-holder'));
     hiddenCards = Array.from(trashbox.querySelectorAll('.js-single-card-holder'));
-    console.log(shownArrayOfCards);
-    console.log(hiddenCards);
+    // console.log(shownArrayOfCards);
+    // console.log(hiddenCards);
   })
 
   selectCategoryFilter.addEventListener('change', elem => {
     let choice = selectCategoryFilter.value;
     console.log(choice);
-    // shownArrayOfCards.forEach(card => {
-    //   if (choice === "All") {
-    //     card.style.display = "block";
-    //     card.style.visibility = "visible";
-    //   } else {
-    //     if (
-    //       card.querySelector('.single-card__main-category').textContent === choice
-    //       || card.querySelector('.single-card__additional-categories').textContent === choice
-    //     ) {
-    //       card.style.display = "block";
-    //       card.style.visibility = "visible";
-    //     } else {
-    //       card.style.display = "none";
-    //       card.style.visibility = "hidden";
-    //     }
-    //   }
-    // });
     if (shownArrayOfCards.length) {
       shownArrayOfCards.forEach(card => {
         if (isAppropriateCard(card) === false) {
@@ -117,7 +83,31 @@ export default async function filterCards() {
     } 
     shownArrayOfCards = Array.from(cardsHolder.querySelectorAll('.js-single-card-holder'));
     hiddenCards = Array.from(trashbox.querySelectorAll('.js-single-card-holder'));
-    console.log(shownArrayOfCards);
-    console.log(hiddenCards);
+    // console.log(shownArrayOfCards);
+    // console.log(hiddenCards);
+  });
+
+  const resetButton = document.querySelector('.js-reset-filter-button');
+
+  resetButton.addEventListener('click', () => {
+    defaultCardsCheckbox.checked = "true";
+    usersCardsCheckbox.checked = "true";
+    selectCategoryFilter.selectedIndex = 0;
+
+    if (shownArrayOfCards.length) {
+      shownArrayOfCards.forEach(card => {
+        if (isAppropriateCard(card) === false) {
+          trashbox.prepend(card);
+        }
+      })
+    }
+    
+    if (hiddenCards.length) {
+      hiddenCards.forEach(card => {
+        if (isAppropriateCard(card) === true) {
+          cardsHolder.prepend(card);
+        }
+      })
+    }
   });
 }
