b951477e4c6bffb3dd80f70e5d87544c0ce39e25
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-02-25 05:49:43 +0300

    feat: start using coolie for saving cards' categories

--- blocks/add-card-form/getCategoriesFromDatabase.mjs
+++ blocks/add-card-form/getCategoriesFromDatabase.mjs
@@ -1,7 +1,14 @@
 export default async function getCategoriesFromDatabase() {
-  let defaultCategoriesURL = '/server/server.php?categories=default';
-
-  let response = await fetch(defaultCategoriesURL);
-  let data = await response.json();
-  return data;
+  let cookie = document.cookie;
+  if (cookie) {
+    let data = cookie.split(';')[0];
+    return JSON.parse(data.split('=')[1]);
+  } else {
+    let defaultCategoriesURL = '/server/server.php?categories=default';
+    let response = await fetch(defaultCategoriesURL);
+    let data = await response.json();
+    console.log(data);
+    document.cookie = `cards=${JSON.stringify(data)}; path=/;`;
+    return data;
+  }
 }
