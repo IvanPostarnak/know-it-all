d95b0a63e7a5847d3ac44adead99031194398dc2
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-02-25 06:55:17 +0300

    feat: make fields of form to color up based on the chosen category

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,18 @@ import renderCards from './../blocks/single-card/renderCards.mjs'
 import goToTopAndBottomButton from '../blocks/go-to-button/goToTopAndBottomButton.mjs';
 import filterCards from './filter.mjs';
 
-addCardForm();
+await addCardForm();
+
+const mainSelect = document.querySelector('select');
+mainSelect.addEventListener('change', () => {
+  const formElements = document.querySelector('.js-cardForm').children;
+  for (let id in formElements) {
+    if (formElements[id].tagName != "DIV") {
+      formElements[id].style.border=`2px solid ${mainSelect.options[mainSelect.selectedIndex].style.color}`;
+    }
+  }
+});
+
 await renderCards();
 goToTopAndBottomButton();
 filterCards();
